8d7d424285e410378cee509e5e7c459b1240bfe5
Robert Paul <rob.k.paul@gmail.com>
2019-03-05 08:55:14 -0800

    Trying to get the responder to work, will then move on to getting commands working.

--- src/main/java/me/robkpaul/www/Main.java
+++ src/main/java/me/robkpaul/www/Main.java
@@ -6,20 +6,20 @@ import org.javacord.api.DiscordApiBuilder;
 public class Main {
 
     public static void main(String[] args) {
-        // Insert your bot's token here
-        String token = "NTQ2MDE3OTM2NDUyNjE2MjAw.D1b_aA.SAyWr81MM2i0e20a9YlOc0VFuZ8";
+        String token = "NTQ2MDE3OTM2NDUyNjE2MjAw.D1b_aA.SAyWr81MM2i0e20a9YlOc0VFuZ8"; // bot token
 
         DiscordApi api = new DiscordApiBuilder().setToken(token).login().join();
 
         // Add a listener which answers with "Pong!" if someone writes "!ping"
         api.addMessageCreateListener(event -> {
-            if (event.getMessage().getContent().equalsIgnoreCase("!ping")) {
-                event.getChannel().sendMessage("Pong!");
+            if (event.getMessage().getContent().substring(0, 5).equalsIgnoreCase("!pong")) { // if the content of the message contains "!pong"
+
+                event.getChannel().sendMessage("Pong!"); // send message "Pong!"
+                System.out.println("Replied to "+ event.getMessageAuthor().getDisplayName());
             }
         });
 
         // Print the invite url of your bot
-        System.out.println("You can invite the bot by using the following url: " + api.createBotInvite());
     }
 
 }
