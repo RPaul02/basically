2a3d4637b5b2170f7db29cd1979602e1ef8fdfec
Robert Paul <rob.k.paul@gmail.com>
2019-03-08 06:32:42 -0800

    Working towards finishing the Split command

--- src/main/java/me/robkpaul/www/Main.java
+++ src/main/java/me/robkpaul/www/Main.java
@@ -2,9 +2,12 @@ package me.robkpaul.www;
 
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
-import org.javacord.api.entity.channel.VoiceChannel;
+import org.javacord.api.entity.channel.ServerVoiceChannel;
 import org.javacord.api.entity.server.Server;
 import org.javacord.api.entity.user.User;
+
+import java.util.Collection;
+import java.util.Optional;
 
 public class Main {
 
@@ -13,33 +16,50 @@ public class Main {
 
         DiscordApi api = new DiscordApiBuilder().setToken(token).login().join();
 
+        Optional<ServerVoiceChannel> redOptional = api.getServerVoiceChannelById("553428495041036299");
+        Optional<ServerVoiceChannel> blueOptional = api.getServerVoiceChannelById("553428606454595600");
+
+        ServerVoiceChannel redChannel = null
+                ;
+        ServerVoiceChannel blueChannel = null;
+
+        if(redOptional.isPresent())
+            redChannel = redOptional.get();
+
+        if(blueOptional.isPresent())
+            blueChannel = blueOptional.get();
+
         // Add a listener which answers with "Pong!" if someone writes "!ping"
         api.addMessageCreateListener(event -> {
             String message = event.getMessage().getContent();
             User author = event.getMessageAuthor().asUser().get();
             Server currentServer = event.getServer().get();
             if (message.length() >= 1 && message.substring(0, 1).equalsIgnoreCase(":")) {
-                /* :song Command */
+                /* :pong Command
+                 *  format - :pong @user
+                 * */
                 if (message.substring(1, 5).equalsIgnoreCase("pong")) {
                     if (!event.getMessage().getMentionedUsers().isEmpty()) {
                         event.getChannel().sendMessage(event.getMessage().getMentionedUsers().get(0).getMentionTag());
                         System.out.println("Replied to " + event.getMessageAuthor().getDisplayName() + "and pinged" + event.getMessage().getMentionedUsers().get(0).getDiscriminatedName());
                     } else {
-                        event.getChannel().sendMessage("```Incorrect Format used. Format is :pong [user] it's important to also not that pinging more than one person will not work.```");
+                        event.getChannel().sendMessage("```Incorrect Format used. Format is :pong [user] it's important to note that ponging more than one person at once will not work.```");
                     }
                 }
-                /* :split Command */
+                /* :split Command
+                 *  format - :split
+                 * */
                 if (message.substring(1, 6).equalsIgnoreCase("split")) {
                     if(author.getConnectedVoiceChannel(currentServer).isPresent()) {
                         if(author.getConnectedVoiceChannel(currentServer).get().getConnectedUsers().size() > 1) {
-                            Object[] connected = author.getConnectedVoiceChannel(currentServer).get().getConnectedUsers().toArray();
-                            for(int i = 0; i<connected.length; i++){
-                                connected[i].equals("lol");
+                            Collection<User> u = author.getConnectedVoiceChannel(currentServer).get().getConnectedUsers();
+                            for (User t : u){
+                                //t.move(redChannel);
                             }
                         }
                     }
                     else {
-                        event.getChannel().sendMessage("```You have to be connected to a channel to use this command dumb dumb.```");
+                        event.getChannel().sendMessage("```You have to be connected to a channel to use this command```");
                     }
 
                 }
