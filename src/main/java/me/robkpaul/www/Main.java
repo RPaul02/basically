e17eae528ebda2f14fe816cee46481744bd0b0b7
Robert Paul <rob.k.paul@gmail.com>
2019-03-12 20:16:47 -0700

    Finished the Embed for -split

--- src/main/java/me/robkpaul/www/Main.java
+++ src/main/java/me/robkpaul/www/Main.java
@@ -6,8 +6,9 @@ import org.javacord.api.entity.channel.ServerVoiceChannel;
 import org.javacord.api.entity.message.embed.EmbedBuilder;
 import org.javacord.api.entity.server.Server;
 import org.javacord.api.entity.user.User;
+import org.javacord.api.util.logging.ExceptionLogger;
 
-import java.awt.*;
+import java.awt.Color;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
@@ -41,58 +42,68 @@ public class Main {
                         int i;
                         try {
                             i = Integer.parseInt(message.substring(6, 7));
-                        }catch(NumberFormatException e){
-                            i=1;
+                        } catch (NumberFormatException e) {
+                            i = 1;
                         }
-                        for(int j = 0; j<i; j++){
+                        for (int j = 0; j < i; j++) {
                             event.getChannel().sendMessage(event.getMessage().getMentionedUsers().get(0).getMentionTag());
                         }
                         System.out.println("Replied to " + event.getMessageAuthor().getDisplayName() + "and pinged" + event.getMessage().getMentionedUsers().get(0).getDiscriminatedName());
                     } else {
                         event.getChannel().sendMessage("```Incorrect Format used. Format is :pong [user] it's important to note that ponging more than one person at once will not work.```");
                     }
-                }
-                else if (message.length() >= 6 && message.substring(1, 6).equalsIgnoreCase("split")) {
+                } else if (message.length() >= 6 && message.substring(1, 6).equalsIgnoreCase("split")) {
                     if (author.getConnectedVoiceChannel(currentServer).isPresent()) {
-                        if (author.getConnectedVoiceChannel(currentServer).get().getConnectedUsers().size() > 1 && redOptional.isPresent() && blueOptional.isPresent()){
+                        if (author.getConnectedVoiceChannel(currentServer).get().getConnectedUsers().size() >= 1 && redOptional.isPresent() && blueOptional.isPresent()) {
                             Collection<User> userCollection = author.getConnectedVoiceChannel(currentServer).get().getConnectedUsers();
                             ArrayList<User> userArrayList = new ArrayList<>(0);
                             userArrayList.addAll(userCollection);
                             Collections.shuffle(userArrayList);
-                            EmbedBuilder builder = new EmbedBuilder().setTitle("Teams").setColor(Color.black);
-                            ArrayList<User> redTeam = new ArrayList<>(0);
-                            ArrayList<User> blueTeam = new ArrayList<>(0);
-                            for(int i = 0; i < userArrayList.size(); i++){
-                                if(i%2==0){
+
+                            String redTeam = "";
+                            String blueTeam = "";
+
+                            for (int i = 0; i < userArrayList.size(); i++) {
+                                if (i % 2 == 0) {
                                     userArrayList.get(i).move(redOptional.get());
-                                    redTeam.add(userArrayList.get(i));
-                                }
-                                else {
+                                    redTeam = redTeam.concat(userArrayList.get(i).getDiscriminatedName());
+                                    if(i<userArrayList.size()-2){
+                                        redTeam = redTeam.concat(", ");
+                                    }
+                                } else {
                                     userArrayList.get(i).move(blueOptional.get());
-                                    blueTeam.add(userArrayList.get(i));
+                                    blueTeam = blueTeam.concat(userArrayList.get(i).getDiscriminatedName().concat(", "));
+                                    if(i<userArrayList.size()-2){
+                                        blueTeam = blueTeam.concat(", ");
+                                    }
                                 }
                             }
-                            builder.addField("Red Team", teamToString(redTeam)).addField("Blue Team", teamToString(blueTeam));
-                            event.getChannel().sendMessage(builder);
+                            if(redTeam.equals(""))
+                                redTeam = "Empty";
 
+                            if(blueTeam.equals(""))
+                                blueTeam = "Empty";
+
+                            EmbedBuilder builder = new EmbedBuilder()
+                                    .setTitle("Teams")
+                                    .setDescription("Teams set by the Split command.")
+                                    .setColor(Color.black)
+                                    .addField("Red Team", redTeam)
+                                    .addField("Blue Team", blueTeam);
+                            event.getChannel().sendMessage(builder);
+                            System.out.println("Blue Team: " + blueTeam + " Red Team: " + redTeam);
 
                         }
                     } else {
-                        EmbedBuilder builder = new EmbedBuilder().setTitle("Command Error").setDescription("You need to be in a channel to use this command").addField("Sidenote", "Yuck").setColor(Color.RED);
+                        EmbedBuilder builder = new EmbedBuilder()
+                                .setTitle("Command Error")
+                                .setDescription("You need to be in a channel to use this command")
+                                .setColor(Color.red);
                         event.getChannel().sendMessage(builder);
                     }
 
                 }
             }
         });
-    }
-
-    public static String teamToString(ArrayList<User> team){
-        String val = "";
-        for(User u : team){
-            val = val + "\n" + u.getDiscriminatedName();
-        }
-
-        return val;
     }
 }
