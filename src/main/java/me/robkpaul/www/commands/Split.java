1defe10047f618fcec6de145008042a227b195b7
Robert Paul <rob.k.paul@gmail.com>
2019-05-23 10:03:50 -0700

    Converting split command to class.

--- src/main/java/me/robkpaul/www/commands/Split.java
+++ src/main/java/me/robkpaul/www/commands/Split.java
@@ -1,4 +1,18 @@
 package me.robkpaul.www.commands;
 
+import org.javacord.api.entity.channel.VoiceChannel;
+import org.javacord.api.entity.message.Message;
+import org.javacord.api.entity.user.User;
+
 public class Split {
+    private Message message;
+    private User author;
+    private VoiceChannel redVoice;
+    private VoiceChannel blueVoice;
+    public Split(Message msg, VoiceChannel rVC, VoiceChannel bVC){
+        message = msg;
+        author = msg.getUserAuthor().isPresent() ? msg.getUserAuthor().get() : null;
+        redVoice = rVC;
+        blueVoice = bVC;
+    }
 }
